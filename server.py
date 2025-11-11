5bcf861050247ef0771a5a8b1aa6381e1620a706
Splendid Cherry <cherry.weiqichu08@berkeley.edu>
2025-11-10 18:46:10 -0800

    implemented upload and solve APIs

--- server.py
+++ server.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI, File, UploadFile
-from typing_extensions import Annotated
 import uvicorn
-from utils import *
+from utils import create_graph_from_json
 from dijkstra import dijkstra
 
 # create FastAPI app
@@ -14,9 +13,8 @@ active_graph = None
 async def root():
     return {"message": "Welcome to the Shortest Path Solver!"}
 
-
 @app.post("/upload_graph_json/")
-async def create_upload_file(file: UploadFile):
+async def create_upload_file(file: UploadFile = File(...)):
     global active_graph
 
     if not file.filename.endswith('.json'):
@@ -29,20 +27,16 @@ async def create_upload_file(file: UploadFile):
     except Exception as e:
         return {"Upload Error": str(e)}
 
-
 @app.get("/solve_shortest_path/start_node_id={start_node_id}&end_node_id={end_node_id}")
 async def get_shortest_path(start_node_id: str, end_node_id: str):
     global active_graph
 
- 
     if active_graph is None:
         return {"Solver Error": "No active graph, please upload a graph first."}
 
-   
     if start_node_id not in active_graph.nodes or end_node_id not in active_graph.nodes:
         return {"Solver Error": "Invalid start or end node ID."}
 
-   
     start_node = active_graph.nodes[start_node_id]
 
     dijkstra(active_graph, start_node)
@@ -67,7 +61,6 @@ async def get_shortest_path(start_node_id: str, end_node_id: str):
         "shortest_path": path,
         "total_distance": end_node.dist
     }
-
 
 if __name__ == "__main__":
     print("Server is running at http://localhost:8080")
